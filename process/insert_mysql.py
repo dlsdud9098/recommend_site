e0a8599913db11c062ffcff4c68774fa7390ce8c
Apic <dlsdud9098@naver.com>
2025-06-20 17:16:13 +0900

    asdf

--- process/insert_mysql.py
+++ process/insert_mysql.py
@@ -15,6 +15,33 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
+    # 테이블 초기화
+    table_name = "novels"
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    
+    # 테이블 재생성
+    create_table_sql = f"""
+    CREATE TABLE {table_name} (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        url VARCHAR(255),
+        img VARCHAR(255),
+        title VARCHAR(255),
+        author VARCHAR(255),
+        recommend INT,
+        genre VARCHAR(255),
+        serial VARCHAR(255),
+        publisher VARCHAR(255),
+        summary TEXT,
+        page_count INT,
+        page_unit VARCHAR(10),
+        age VARCHAR(10),
+        platform VARCHAR(255),
+        keywords TEXT,
+        viewers INT
+    )
+    """
+    cursor.execute(create_table_sql)
+
     insert_sql = """
     INSERT INTO novels (url, img, title, author, recommend, genre, serial, publisher, summary, page_count, page_unit, age, platform, keywords, viewers) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
@@ -28,50 +55,42 @@ if __name__ == '__main__':
             img = data['img']
             title = data['title']
             author = data['author']
-            recommend = int(data['recommend'].replace(',',''))
+            recommend = int(data['recommend'].replace(',', ''))
             genre = data['genre']
             serial = data['serial']
             publisher = data['publisher']
             summary = data['summary']
             page_unit = data['page_unit']
-
-            
-            if page_unit == '권':
-                
-                page_count = int(data['page_count'].replace('회차 ', '').replace(',', ''))*25, 
-                page_unit = '화'
-            else:
-                page_count = int(data['page_count'].replace('회차', '').replace(',', '')), 
-            
+            page_count = data['page_count']
             age = data['age']
             platform = data['platform']
             keywords = data['keywords']
             viewers = int(data['viewers'].replace(',', ''))
 
             cursor.execute(insert_sql, (
-                                    url,
-                                    img,
-                                    title,
-                                    author,
-                                    recommend,
-                                    genre,
-                                    serial,
-                                    publisher,
-                                    summary,
-                                    page_count,
-                                    page_unit,
-                                    age,
-                                    platform,
-                                    keywords,
-                                    viewers,)
-            )
+                url,
+                img,
+                title,
+                author,
+                recommend,
+                genre,
+                serial,
+                publisher,
+                summary,
+                page_count,
+                page_unit,
+                age,
+                platform,
+                keywords,
+                viewers
+            ))
         except Exception as e:
             import traceback
             print(data)
             print(e)
             traceback.print_exc()
             break
-        
+
     db.commit()
 
     db.close()
--- process/preprocessing.py
+++ process/preprocessing.py
@@ -14,47 +14,68 @@ def save_data(datas):
         with open('data/all_data.data', 'wb') as f:
             pickle.dump(data, f)
 
+        os.remove('data/all_data.json')
+
     elif isinstance(datas, list):
         with open('data/all_data.data', 'wb') as f:
             pickle.dump(datas, f)
 
 
 def load_data():
-    if os.path.exists('data/all_data.data'):
-        with open('data/all_data.data', 'rb') as f:
-            all_data = pickle.load(f)
-    else:
-        files = glob('data/*.data')
+    files = glob('data/*.data')
 
-        all_data = []
-        for file in files:
-            with open(file, 'rb') as f:
-                data = pickle.load(f)
-                all_data.extend(data)
+    all_data = []
+    for file in files:
+        with open(file, 'rb') as f:
+            data = pickle.load(f)
+            all_data.extend(data)
 
-        save_data(all_data)
-        os.remove('data/all_data.json')
+    save_data(all_data)
+    # os.remove('data/all_data.json')
 
     return all_data
-
 
 def drop_dupl(df:pd.DataFrame):
     new_df = df.drop_duplicates(['url'])
     new_df = new_df.drop_duplicates(['title'])
     return new_df
 
+# keywords 처리
+def process_keywords(keywords):
+    if isinstance(keywords, list):  # 리스트인 경우
+        return "#" + " #".join(keywords)
+    elif isinstance(keywords, str):  # 문자열인 경우
+        return "#" + " #".join(keywords.split())
+    return ""  # 다른 데이터 타입은 빈 문자열로 처리
+
 def str_preprocessing(df:pd.DataFrame):
-    
+    df = df[df.notnull()]
     df['title'] = df['title'].str.strip()
     df['summary'] = df['summary'].str.replace(r'[\r, \n, \t]',' ', regex = True).replace(r'\s{2,}', ' ', regex=True).str.strip()
-    df['keywords'] = df['keywords'].str.replace(r'[\r, \n, \t]',' ', regex = True).replace(r'\s{2,}', ' ', regex=True).str.strip()
+    # df['keywords'] = df['keywords'].str.replace(r'[\r, \n, \t]',' ', regex = True).replace(r'\s{2,}', ' ', regex=True).str.strip()
+    # df['keywords'] = df['keywords'].apply(process_keywords)
+
+    # df['keywords'] = df['keywords'].apply(lambda x: str(x) if isinstance(x, list) else x)
+
+    df.loc[df['page_unit'] == '권', 'page_unit'] = '회차'
+    df.loc[df['page_unit'] == '화', 'page_count'] *= 25
+
 
 
     save_data(df)
+    return df
 if __name__ == '__main__':
     datas = load_data()
     df = pd.DataFrame(datas)
+    # print(df.head())
 
     df = drop_dupl(df)
 
-    str_preprocessing(df)
+    df = str_preprocessing(df)
+
+    # print(str(row['keywords']) if isinstance(row['keywords'], list) else row['keywords'])
+
+    for i, row in df.iterrows():
+        pass
+
+    print(row['keywords'])
